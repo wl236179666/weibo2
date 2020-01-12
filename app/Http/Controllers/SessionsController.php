c70a39df3b320faea57fd81ffea2d7e00c506c78
wangliang <236179666@qq.com>
2020-01-12 23:54:53 +0000

    用户退出登录

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -15,8 +15,9 @@ class SessionsController extends Controller
     public function store(Request $request)
     {
         $credentials = $this -> validate($request,[
-            'email' => 'required|email|max:255',
-            'password' => 'required'
+            'password' => 'required',
+            'email' => 'required|email|max:255'
+
         ]);
 
         if(Auth::attempt($credentials)){
@@ -26,5 +27,12 @@ class SessionsController extends Controller
             session() -> flash('danger','很抱歉，您的邮箱和密码不匹配');
             return redirect() -> back() -> withInput();
         }
+    }
+
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flash('success', '您已成功退出！');
+        return redirect('login');
     }
 }
